bb907a55698fc7944b26288e5fa09fc934f2a0b0
Chris Sell <christophersell3@gmail.com>
2012-01-24 01:06:39 -0600

    rails app frame, first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdReality::Application.config.secret_token = '99b10ef1e59770c5914bd50b627ebf9e5222a36598f6319afab2d1542d9b901d5f308107181a9c1d0c4b41c75edfb3a49afe998681e13ee95bd6e11f5a166748'
